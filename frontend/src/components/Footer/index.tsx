7cd8cba9276aa39f06648fcf77425e564e7b5ba5
Silvio Mori Neto <silviomori@gmail.com>
2021-05-03 20:12:47 -0300

    Create basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">Developed by <a href="https://www.linkedin.com/in/silviomori" target="_blank" rel="noreferrer">Silvio Mori</a></p>
+                <p className="text-light"><small><strong>Spring React Week</strong><br />
+    Promoted by DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
