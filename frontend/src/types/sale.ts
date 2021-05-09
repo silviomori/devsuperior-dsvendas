4368690dc645635d0672b6ac165d2595cdc5818c
Silvio Mori Neto <silviomori@gmail.com>
2021-05-09 16:17:23 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { SellerDTO } from "./seller"
+
+export type SaleDTO = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: SellerDTO;
+}
+
+export type SalePageDTO = {
+    content?: SaleDTO[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
 export type SaleSumDTO = {
     sellerName: string;
     sum: number;
---
+++ frontend/src/types/seller.ts
@@ -0,0 +1,4 @@
+export type SellerDTO = {
+    id: number;
+    name: string;
+}
