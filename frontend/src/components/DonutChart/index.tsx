22b0c616584af18b85f7793a07cd8078a3d4b169
Silvio Mori Neto <silviomori@gmail.com>
2021-05-09 12:13:36 -0300

    First request to server using Axios

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,12 +1,26 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSumDTO } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 const DonutChart = () => {
 
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
-    
+    // TODO: Use react hook
+    let chartData: ChartData = {labels: [], series: []};
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then(response => {
+            const data = response.data as SaleSumDTO[];
+            const labels = data.map(x => x.sellerName);
+            const series = data.map(x => x.sum);
+            chartData = {labels: labels, series: series};
+            console.log(chartData);
+        });
+
     const options = {
         legend: {
             show: true
@@ -15,8 +29,8 @@ const DonutChart = () => {
 
     return (
         <Chart
-            options={{...options, labels: mockData.labels}}
-            series={mockData.series}
+            options={{...options, labels: chartData.labels}}
+            series={chartData.series}
             type="donut"
             height="240"
         />
---
+++ frontend/src/types/sale.ts
@@ -0,0 +1,4 @@
+export type SaleSumDTO = {
+    sellerName: string,
+    sum: number
+}
