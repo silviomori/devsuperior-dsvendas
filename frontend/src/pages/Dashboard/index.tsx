40394ab23ef017314674e4dec6bf45cf57e01059
Silvio Mori Neto <silviomori@gmail.com>
2021-05-09 11:08:05 -0300

    Configure routes Home and Dashboard

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,35 @@
+import BarChart from "components/BarChart";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+const Dashboard = () => {
+    return (
+        <>
+            <NavBar />
+            <div className="container">
+                <h1 className="text-primary py-3">Dashboard</h1>
+
+                <div className="row px-3">
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Success rate (%)</h5>
+                        <BarChart />
+                    </div>
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Sales</h5>
+                        <DonutChart />
+                    </div>
+                </div>
+                <div className="py3">
+                    <h2 className="text-primary">Sales</h2>
+                    <DataTable />
+                </div>
+
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default Dashboard;
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+            <div className="container">
+                <h1>Home Page</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
