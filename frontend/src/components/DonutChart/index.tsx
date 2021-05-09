6929286456314c4d5e6c9deba16fa3d6b4f12909
Silvio Mori Neto <silviomori@gmail.com>
2021-05-09 13:01:49 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSumDTO } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,16 +11,17 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    // TODO: Use react hook
-    let chartData: ChartData = {labels: [], series: []};
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSumDTO[];
-            const labels = data.map(x => x.sellerName);
-            const series = data.map(x => x.sum);
-            chartData = {labels: labels, series: series};
-            console.log(chartData);
-        });
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSumDTO[];
+                const labels = data.map(x => x.sellerName);
+                const series = data.map(x => x.sum);
+                setChartData({ labels: labels, series: series });
+            });
+    }, []);
 
     const options = {
         legend: {
@@ -29,7 +31,7 @@ const DonutChart = () => {
 
     return (
         <Chart
-            options={{...options, labels: chartData.labels}}
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
